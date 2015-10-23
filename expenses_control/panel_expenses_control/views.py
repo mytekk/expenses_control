44fa0e64824a84ab058eede714fd5dd550f81914
mytek <bartlomiej.mytko@gmail.com>
2015-10-23 14:09:47 +0200

    Strona glowna projektu (napis) i aplikacji (lista kategorii)

--- expenses_control/panel_expenses_control/views.py
+++ expenses_control/panel_expenses_control/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from .models  import *
 
-# Create your views here.
+
+def index(request):
+	lista_kategorii = Kategoria.objects.all()
+	template = loader.get_template('panel_expenses_control/index.html')
+	context = RequestContext(request, {'lista_kategorii': lista_kategorii,})
+	return HttpResponse(template.render(context))
