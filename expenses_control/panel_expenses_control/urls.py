3f32c636eeee0f60815e3b53636fa734560710ba
mytek <bartlomiej.mytko@gmail.com>
2016-01-15 10:58:12 +0100

    Dopracowanie wyglądu aplikacji (#1).

--- expenses_control/panel_expenses_control/urls.py
+++ expenses_control/panel_expenses_control/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     url(r'^(?P<rok>[0-9]+)/(?P<miesiac>[0-9]+)/$', views.index, name='index'),
     url(r'^nowywydatek$', views.nowywydatek, name='nowywydatek'),
     url(r'^podaj_podkategorie/(?P<kat_id>[0-9]+)$', views.podaj_podkategorie, name='podaj_podkategorie'),
+    url(r'^about$', views.about),
+    url(r'^post$', views.post),
+    url(r'^contact$', views.contact),
     ]
